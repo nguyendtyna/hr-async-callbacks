4b3efd583fc7aedd5acef55b39fb1b8ec153a85b
Tye <tyemacon@gmail.com>
2020-03-11 11:58:35 -0500

    fixed comments for TOM

--- callback-prompts/client/src/callbackReview.js
+++ callback-prompts/client/src/callbackReview.js
@@ -1,12 +1,12 @@
 const FILL_ME_IN = "FILL ME IN!";
-// Callback Review!
-
-// The following functions contain examples of synchronous callback functions
-// Not all functions end with a return statement!
-// Instead, some functions pass data along to the next function by invoking
-// a callback function with the data as the argument.
-
-// Consider the following example:
+/**
+ * The following functions contain examples of synchronous callback functions
+ * Not all functions end with a return statement!
+ * Instead, some functions pass data along to the next function by invoking
+ * a callback function with the data as the argument.
+ *
+ * Consider the following example:
+ */
 function findHalfWordLength(word, callback) {
   let halfWordLength = Math.floor(word.length / 2);
   callback(halfWordLength);
@@ -17,43 +17,50 @@ function print(output) {
 }
 
 findHalfWordLength("output-word", print); // notice that print is NOT invoked when passed as an argument!
-// Instead of telling findHalfWordLength to return a piece of data to us right away,
-// we provide it with a callback function to use with the data it generates.
-// This example may be a little contrived, but the same structure is used for much
-// more complex chains of functions.
-
-// Fill out the following higher-order functions as specified.
-// Feel free to create any helper functions deemed necessary.
-
-// Given n, generate an array of values corresponding to each
-// magnitude level and digit | n: 7134 -> [7000, 100, 30, 4]
-// makeDigitArray should invoke the callback function its provided
-// with the generated array passed as an argument.
+/**
+ * Instead of telling findHalfWordLength to return a piece of data to us right away,
+ * we provide it with a callback function to use with the data it generates.
+ * This example may be a little contrived, but the same structure is used for much
+ * more complex chains of functions.
+ *
+ * Fill out the following higher-order functions as specified.
+ * Feel free to create any helper functions deemed necessary.
+ *
+ *  Given n, generate an array of values corresponding to each
+ * magnitude level and digit | n: 7134 -> [7000, 100, 30, 4]
+ * makeDigitArray should invoke the callback function its provided
+ * with the generated array passed as an argument.
+ */
 function makeDigitArray(n, callback) {
   // Your work here!
 }
 
-// Given an input array, find the sum of elements in the following manner:
-// ADD elements found at even indices,
-// SUBTRACT elements found at odd indices.
-// EX: arr = [7000, 100, 30, 4] -> (7000 - 100 + 30 - 4) = 6926
+/**
+ * Given an input array, find the sum of elements in the following manner:
+ * ADD elements found at even indices,
+ * SUBTRACT elements found at odd indices.
+ * EX: arr = [7000, 100, 30, 4] -> (7000 - 100 + 30 - 4) = 6926
+ */
 function arraySummer(arr) {
   // Your work here!
   let sum;
 
-  // We can use anonymous functions for callbacks too!
-  // The second parameter of primeFactors is a callback function,
-  // Instead of explicitly defining it somewhere else to be garbage collected later,
-  // Let's just define the function on the fly and pass it in-line!
-  // !!!!!!!    ES5 or ES6?    !!!!!!!!!
+  /**
+   * We can use anonymous functions for callbacks too!
+   * The second parameter of primeFactors is a callback function,
+   * Instead of explicitly defining it somewhere else to be garbage collected later,
+   * Let's just define the function on the fly and pass it in-line!
+   */
   primeFactors(sum, function() {
-    // Many common iterators use synchronous callback function as well,
-    // Provide the array.map method below with an anonymous callback function
-    // that implements the following functionality:
-
-    // The new array entries are strings describing each number and it's square
-    // EXAMPLE: array = [2, 3463]
-    // --> mappedArray = ['The square of 2 is 4', 'The square of 3463 is 11992369']
+    /**
+     * Many common iterators use synchronous callback function as well,
+     * Provide the array.map method below with an anonymous callback function
+     * that implements the following functionality:
+     *
+     * The new array entries are strings describing each number and it's square
+     * EXAMPLE: array = [2, 3463]
+     * --> mappedArray = ['The square of 2 is 4', 'The square of 3463 is 11992369']
+     */
     let mappedNums;
 
     // This block of code is strictly used for testing purposes
@@ -63,10 +70,12 @@ function arraySummer(arr) {
   });
 }
 
-// Generate an array of all prime factors for the given number.
-// EXAMPLE: num = 6926 -> [2, 3463]
-// primeFactors should pass the generated array to the callback
-// function it's provided.
+/**
+ * Generate an array of all prime factors for the given number.
+ * EXAMPLE: num = 6926 -> [2, 3463]
+ * primeFactors should pass the generated array to the callback
+ * function it's provided.
+ */
 function primeFactors(num, callback) {
   // Your work here!
 }
